497368161e36004439a66a4ed5cbe2fd64c5c5a1
syyad <syyadkhan@yahoo.com>
2021-07-11 10:53:32 -0400

    useEffect

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,29 @@ import { API, withSSRContext } from "aws-amplify";
 import { listParks } from "../src/graphql/queries";
 import { AmplifyS3Image } from "@aws-amplify/ui-react";
 import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-export async function getStaticProps() {
-  const SSR = withSSRContext();
-  const { data } = await SSR.API.graphql({ query: listParks });
-  return {
-    props: {
-      parks: data.listParks.items,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const SSR = withSSRContext();
+//   const { data } = await SSR.API.graphql({ query: listParks });
+//   return {
+//     props: {
+//       parks: data.listParks.items,
+//     },
+//   };
+// }
 
 export default function Home({ parks }) {
+  const [parks, setParks] = useState([]);
+
+  useEffect(() => {
+    const fetchParks = async () => {
+      const { data } = await API.graphql({ query: listParks });
+      setParks(data.listParks.items);
+    };
+    fetchParks();
+  }, []);
+
   return (
     <div>
       <Head>
@@ -22,7 +33,7 @@ export default function Home({ parks }) {
       </Head>
       <div className="container">
         <h1>
-          National Parks <Link href="/create">(+)</Link>
+          National Parks!!!!!!!!!!!!!!!! <Link href="/create">(+)</Link>
         </h1>
         <div className="img-grid">
           {parks.map((park) => {
